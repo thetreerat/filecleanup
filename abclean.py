269a50e5dab9daff4ca0af7259e731e1ab27b6b4
Harold Clark <thetreerat@gmail.com>
2015-10-09 22:37:22 -0400

    nightly changes store

--- abclean.py
+++ abclean.py
@@ -7,13 +7,26 @@ def mainscreenmessage(c=0, usb=0):
 
      %i files in C: dir
      %i files on USB dir""" % (c, usb))
-     
+
+def countbackups(path=""):
+    count = 0
+    try:
+        filelist = os.listdir(path)
+        for f in filelist:
+            if f[-3:] == '.py':
+                count += 1
+        return count
+    except FileNotFoundError as e:
+        print("""%s Path not found """ % (path))
+        return None
 
 if __name__ == '__main__':
     try:
         print("Hello, on to more importan things")
-        filelist = os.listdir()
-        filecount = len(filelist)
-        mainscreenmessage(filecount,1)
+        
+        
+        filecountc = countbackups(path="""c:\\users\\Harold\\Documents\\""")
+        filecountusb = countbackups(path="""c:\\users\\Harold\\""")
+        mainscreenmessage(filecountc,filecountusb)
     finally:
         print("Thank you for playing")
